c4ac7e5f5e0264898d1111dda85ef681193c8e74
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-11 10:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
+
+class Person{
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	function greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
 }
 
-Person.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-};
-
-Employee.prototype = Object.create(Perosn.prototype);
-
-
-function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle = jobTitle;
+class Employee extends Person{
+	constructor(name, age, jobTitle) {
+		super(make, model);
+		this.jobTitle = jobTitle;
+	}
+	function jobGreet(){
+		    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	}
 }
 
-Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old. and my jon title is ${this.jobTitle}.`)
-};
+
+
+
 
 // Do not change code below this line
 window.Person = Person;
