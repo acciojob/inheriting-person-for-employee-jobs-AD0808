5b9fe0ba6c798f68e55c65943dbc0e9bd40bb938
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-11 10:45:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+};
+
+Employee.prototype = Object.create(Perosn.prototype);
+
+
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
+	this.jobTitle = jobTitle;
+}
+
+
+
+Employee.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+};
 
 // Do not change code below this line
 window.Person = Person;
