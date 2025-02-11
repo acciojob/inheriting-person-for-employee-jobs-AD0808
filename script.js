287c74232590110737d8e36509594c2f0c4b22ae
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-11 10:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,8 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle = jobTitle;
 }
 
-
-
-Employee.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old. and my jon title is ${this.jobTitle}`)
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old. and my jon title is ${this.jobTitle}.`)
 };
 
 // Do not change code below this line
